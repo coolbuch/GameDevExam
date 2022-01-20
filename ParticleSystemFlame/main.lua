ae672e4098a832ffafd703fb4a0e2f7722683060
coolbuch <kovdima2000@gmail.com>
2022-01-20 17:15:24 +0800

    updated snowflakes and added flame into the fire project

--- ParticleSystemFlame/main.lua
+++ ParticleSystemFlame/main.lua
@@ -1,23 +1,27 @@
-
 function love.load()
-  windForce = 5 -- отрицательное значение ветер влево, положительное вправо
+  windForce = -0.3 -- отрицательное значение ветер влево, положительное вправо
   
-  img = love.graphics.newImage("snowflake.png")
-  pSystem = love.graphics.newParticleSystem(img, 200)
-  pSystem:setParticleLifetime(4,20)
-  pSystem:setRadialAcceleration(-1, 1)
-  pSystem:setEmissionArea("ellipse", love.graphics.getWidth(), 0, 0, false)
+  img = love.graphics.newImage("/sprites/texture.png")
+  firepit_img = love.graphics.newImage("/sprites/fireplace.png")
+  
+  pSystem = love.graphics.newParticleSystem(img, 4000)
+  pSystem:setParticleLifetime(0.5,1)
+  
   pSystem:setLinearAcceleration(windForce * 30, 0, windForce * 100, 0)
-  pSystem:setSizes(0.3, 0.5, 0.7)
-  pSystem:setDirection(math.pi/ 2)
+  pSystem:setSizes(0.3, 0.4, 0.5)
+  pSystem:setDirection(math.pi * 3/ 2)
   pSystem:setSizeVariation(1)
-  pSystem:setSpeed(150)
-  pSystem:setSpin(0, 1)
-  pSystem:setEmissionRate(20)
+  pSystem:setSpeed(50)
+  pSystem:setEmissionArea("normal", 5, 5, 0, false)
+  --pSystem:setSpin(0, 1)
+  pSystem:setColors(237/255, 98/255, 12/255, 1, 242/255, 198/255, 22/255, 1, 1, 1, 1, 1)
+  pSystem:setEmissionRate(800)
 end
 
 function love.draw()
-  love.graphics.draw(pSystem, love.graphics.getWidth() / 2, -70)
+  
+  love.graphics.draw(firepit_img,  love.graphics.getWidth() / 2, 300, -0.2 , 0.1, 0.1)
+  love.graphics.draw(pSystem, love.graphics.getWidth() / 2 + 43, 300)
 end
 
 function love.update(dt)
--- ParticleSystemSnowflakes/main.lua
+++ ParticleSystemSnowflakes/main.lua
@@ -1,18 +1,20 @@
 
 function love.load()
-  windForce = 5 -- отрицательное значение ветер влево, положительное вправо
+  windForce = 0 -- отрицательное значение ветер влево, положительное вправо
   
   img = love.graphics.newImage("snowflake.png")
   pSystem = love.graphics.newParticleSystem(img, 200)
   pSystem:setParticleLifetime(4,20)
   pSystem:setRadialAcceleration(-1, 1)
   pSystem:setEmissionArea("ellipse", love.graphics.getWidth(), 0, 0, false)
-  pSystem:setLinearAcceleration(windForce * 30, 0, windForce * 100, 0)
+  pSystem:setLinearAcceleration(windForce * 15, 0, windForce * 50, 0)
   pSystem:setSizes(0.3, 0.5, 0.7)
   pSystem:setDirection(math.pi/ 2)
   pSystem:setSizeVariation(1)
   pSystem:setSpeed(150)
   pSystem:setSpin(0, 1)
+  pSystem:setTangentialAcceleration(-10, 10)
+  pSystem:setColors(1,1,1,1, 1, 1, 1, 0)
   pSystem:setEmissionRate(20)
 end
 
