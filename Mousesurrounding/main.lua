5c9ccc7b7c51d5b93d177492fb7e2aea4fdd3190
coolbuch <kovdima2000@gmail.com>
2022-01-22 03:20:49 +0800

    task mouse surrounding done

--- Mousesurrounding/main.lua
+++ Mousesurrounding/main.lua
@@ -4,10 +4,33 @@ require "vehicle"
 numberOfVehicles = 50
 
 function love.load()
+  
+  world = love.physics.newWorld(0, 0, true)
+  
+  circleBody = love.physics.newBody(world, love.mouse.getX(), love.mouse.getY(), "kinematic")
+  shape = love.physics.newCircleShape(40)
+  fixture = love.physics.newFixture(circleBody, shape)
+  
+  leftBorderBody = love.physics.newBody(world, 0, 0, "static")
+  leftBorderShape = love.physics.newRectangleShape(20, 10000)
+  leftBorderFixture = love.physics.newFixture(leftBorderBody, leftBorderShape)
+  
+  rightBorderBody = love.physics.newBody(world, love.graphics.getWidth(), 0, "static")
+  rightBorderShape = love.physics.newRectangleShape(20, 10000)
+  rightBorderFixture = love.physics.newFixture(rightBorderBody, rightBorderShape)
+  
+  upBorderBody = love.physics.newBody(world, 0, 0, "static")
+  upBorderShape = love.physics.newRectangleShape(10000, 20)
+  upBorderFixture = love.physics.newFixture(upBorderBody, upBorderShape)
+  
+  bottomBorderBody = love.physics.newBody(world, 0, love.graphics.getHeight(), "static")
+  bottomBorderShape = love.physics.newRectangleShape(10000, 20)
+  bottomBorderFixture = love.physics.newFixture(bottomBorderBody, bottomBorderShape)
+  
   -- создаем массив агентов
   vehicles = {}
   for i = 0, numberOfVehicles do
-    vehicles[i] = Vehicle:create(love.graphics.getWidth() * love.math.random(), love.graphics.getHeight() * love.math.random() )
+    vehicles[i] = Vehicle:create(love.graphics.getWidth() * love.math.random(), love.graphics.getHeight() * love.math.random(), world)
   end
   
  
@@ -21,11 +44,15 @@ function love.draw()
 end
 
 function love.update(dt)
+  
   for i = 0, numberOfVehicles do
     vehicles[i]:update()
-    vehicles[i]:borders()
+    --vehicles[i]:borders()
+    --vehicles[i]:checkCollisionWithOtherVehicles(vehicles, numberOfVehicles)
   end
-  
+  circleBody:setX(love.mouse.getX())
+  circleBody:setY(love.mouse.getY())
+  world:update(dt)
 end
 
 
--- Mousesurrounding/vehicle.lua
+++ Mousesurrounding/vehicle.lua
@@ -1,63 +1,53 @@
-
 Vehicle = {}
 Vehicle.__index = Vehicle
 
-function Vehicle:create(x, y)
+function Vehicle:create(x, y, world)
     local vehicle = {}
     setmetatable(vehicle, Vehicle)
-    vehicle.velocity = Vector:create(love.math.random() *2, love.math.random() *2)
+    vehicle.velocity = Vector:create(0,0)
     vehicle.acceleration = Vector:create(0, 0)
     vehicle.location = Vector:create(x, y)
-    vehicle.r = 4
+    vehicle.r = 5
     vehicle.vertices = {0, -vehicle.r * 2, -vehicle.r, vehicle.r * 2, vehicle.r, vehicle.r * 2}
     width = love.graphics.getWidth()
     height = love.graphics.getHeight()
+    
+    vehicle.body = love.physics.newBody(world, vehicle.location.x, vehicle.location.y, "dynamic")
+    vehicle.shape = love.physics.newRectangleShape(vehicle.r * 2, vehicle.r * 3)
+    vehicle.fixture = love.physics.newFixture(vehicle.body, vehicle.shape)
+    
     vehicle.maxSpeed = 0.0001
     vehicle.maxForce = 0.008
     vehicle.theta = 0
-
+    vehicle.mouseRadius = 20
     return vehicle
 end
 
 function Vehicle:update()
-    self.velocity:add(self.acceleration)
-    self.velocity:limit(self.maxSpeed)
-    self.location:add(self.velocity)
-    self.acceleration:mul(0)
+    self:moveToMouse()
 end
 
 function Vehicle:applyForce(force)
     self.acceleration:add(force)
 end
 
-function Vehicle:borders()
-    if self.location.x < -self.r then
-        self.location.x = width + self.r
-    end
-
-    if self.location.y < -self.r then
-        self.location.y = height + self.r
-    end
-
-    if self.location.x > width + self.r then
-        self.location.x = -self.r
-    end
-
-    if self.location.y > height + self.r then
-        self.location.y = -self.r
-    end
-end
 
 function Vehicle:draw()
-    local theta = self.velocity:heading() + math.pi / 2
     love.graphics.push()
-    love.graphics.translate(self.location.x, self.location.y)
-    love.graphics.rotate(theta)
+    love.graphics.translate(self.body:getX(), self.body:getY())
+    love.graphics.rotate(self.theta)
     love.graphics.polygon("fill", self.vertices)
     love.graphics.pop()
 end
 
 
-
-
-
+function Vehicle:moveToMouse()
+  local mousePos = Vector:create(love.mouse.getX(), love.mouse.getY())
+  local location = Vector:create(self.body:getX(), self.body:getY())
+  --print(tostring(location))
+  dir = mousePos - location
+  dir = dir:norm()
+  self.theta = dir:heading() + math.pi / 2
+  --print(tostring(dir))
+  self.body:setLinearVelocity(dir.x * 100, dir.y * 100)
+end
